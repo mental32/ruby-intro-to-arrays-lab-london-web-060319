db74cc6ea9118303d566089a3f92ac6daa1aa08d
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-26 23:50:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,7 @@ def array_with_two_elements
   [nil] * 2
 end
 
-def first_element(arr)
+def first_element_with_array_methods(arr)
   arr.first
 end
 
@@ -15,6 +15,10 @@ def third_element(arr)
   arr[2]
 end
 
-def last_element(arr)
+def last_element_with_array_methods(arr)
   arr.last
+end
+
+def length_of_array(arr)
+  arr.length
 end
