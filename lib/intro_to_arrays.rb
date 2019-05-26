e84a3bb4f3d41eeb288b1f3705ba78bf9f402324
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-26 23:51:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,8 +11,16 @@ def first_element_with_array_methods(arr)
   arr.first
 end
 
+def first_element(arr)
+  arr[0]
+end
+
 def third_element(arr)
   arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
 end
 
 def last_element_with_array_methods(arr)
